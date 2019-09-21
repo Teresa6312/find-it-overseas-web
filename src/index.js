e9430fff7577ee9b1b571e320d1208a67ad9fd2f
Cuishan Liu <cuishan1122@gmail.com>
2019-09-21 13:56:51 -0400

    set up redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
 import './index.scss';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <BrowserRouter><App /></BrowserRouter>
+    <Provider><BrowserRouter><App /></BrowserRouter></Provider>
     , document.getElementById('root')
     );
 
